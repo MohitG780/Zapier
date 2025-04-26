7fae80d7bc0c7da6a09ffc3e15c42d88f91ac61b
MohitG780 <mohitgupta000780@gmail.com>
2025-04-26 15:18:23 +0530

    hooks backend completed

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,6 +1,32 @@
 import express from "express";
+
 const app =express();
- app.post("/hooks/catch/:userId/:zapId",(req,res)=>{
+app.use(express.json());
+import { PrismaClient,Prisma } from "@prisma/client";
+const client=new PrismaClient();
+ app.post("/hooks/catch/:userId/:zapId",async(req,res)=>{
 const userId=req.params.userId;
 const zapId=req.params.zapId;
+const body=req.body;
+await client.$transaction(async tx=>{
+    const run=await tx.zapRun.create({
+        
+            data:{
+                zapId:zapId,
+                metadata:body
+            }
+
+    });
+    await  tx.zapRunOutbox.create({
+        data:
+        {
+            zapRunId:run.id
+        }
+    })
+})
+res.json({
+    message:"Webhook recieved"
+})
+
  })
+ app.listen(3000);
